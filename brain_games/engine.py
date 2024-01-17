5bddc8bd3bfde2105c3eb30119fa95639d4ee2b8
Lukashhhhh <131288459+Lukashhhhh@users.noreply.github.com>
2024-01-17 09:27:02 +0000

    upgrade engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -2,17 +2,17 @@ from brain_games.cli import welcome_user
 import prompt
 
 
-def run_game(game, description: str):
+def run_game(game):
     name = welcome_user()
-    print(description)
-    flag = False
-    for _ in range(3):
-        qwestion, correct_answer = game()
+    print(game.description)
+    max_game_rounds = 3
+    for _ in range(max_game_rounds):
+        qwestion = game.generate_qwestion()
+        correct_answer = game.correct_answer(qwestion)
         print(f'Question: {qwestion}')
         answer = prompt.string('Your answer: ')
         if answer == correct_answer:
             print('Correct!')
-            flag = True
             continue
         else:
             print(
@@ -20,7 +20,6 @@ def run_game(game, description: str):
                 f"'{correct_answer}'."
             )
             print(f"Let's try again, {name}!")
-            flag = False
             break
-    if flag:
+    else:
         print(f"Congratulations, {name}!")
